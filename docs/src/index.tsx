cd309dcaa33feea789c2f08121d642ae5bb113d2
AyaNaimi <ayanaimi21@gmail.com>
2026-09-05 15:17:14 +0000

    .,

--- docs/src/index.tsx
+++ docs/src/index.tsx
@@ -24,7 +24,7 @@ const container = document.getElementById("root");
 
 const root = createRoot(container as Element);
 root.render(
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
+  <BrowserRouter basename="/Trimly2">
     <StyledEngineProvider injectFirst>
       <ThemeProvider theme={muiTheme}>
         <CssBaseline />
